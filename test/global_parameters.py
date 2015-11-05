be13d9dfc90caa07d9a9fb39a9ef725370d8213a
Filippo Bonazzi <filippo.bonazzi@aalto.fi>
2015-11-05 20:59:27 +0200

    Scorporated global test parameters in dedicated file

---
+++ test/global_parameters.py
@@ -0,0 +1,40 @@
+#
+#    Written by Filippo Bonazzi
+#    Copyright (C) 2015 Aalto University
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 2 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+"""Global test parameters and functions."""
+import os.path
+
+BASE_DIR_GLOBAL = "test/test_policy_files"
+MACROFILES_GLOBAL = [
+    "global_macros",
+    "te_macros",
+    "ioctl_macros"]
+POLICYFILES_GLOBAL = [
+    "rules.te"]
+SUPPORTED_MACRO_FILES_GLOBAL = [
+    "global_macros",
+    "te_macros"]
+EXISTING_PLUGINS_GLOBAL = [
+    "global_macros",
+    "te_macros"]
+VALID_PLUGINS_GLOBAL = [
+    "global_macros",
+    "te_macros"]
+
+
+def join_files(basedir, files):
+    return [os.path.join(os.path.expanduser(basedir), x) for x in files if x]
