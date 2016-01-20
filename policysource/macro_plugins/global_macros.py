c6be8330fb833c6af7e588e8183d07a36dc4c745
Filippo Bonazzi <filippo.bonazzi@aalto.fi>
2016-01-20 18:28:40 +0200

    Updated licensing information in the policysource library files

--- policysource/macro_plugins/global_macros.py
+++ policysource/macro_plugins/global_macros.py
@@ -1,19 +1,22 @@
 #
 #    Written by Filippo Bonazzi
-#    Copyright (C) 2015 Aalto University
+#    Copyright (C) 2016 Aalto University
+#
+#    This file is part of the policysource library.
 #
 #    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU General Public License as published by
-#    the Free Software Foundation, either version 2 of the License, or
-#    (at your option) any later version.
+#    it under the terms of the GNU Lesser General Public License as
+#    published by the Free Software Foundation, either version 2.1 of
+#    the License, or (at your option) any later version.
 #
 #    This program is distributed in the hope that it will be useful,
 #    but WITHOUT ANY WARRANTY; without even the implied warranty of
 #    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #    GNU General Public License for more details.
 #
-#    You should have received a copy of the GNU General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#    You should have received a copy of the GNU Lesser General Public
+#    License along with this program.  If not, see
+#    <http://www.gnu.org/licenses/>.
 #
 """Plugin to parse the global_macros file"""
 
