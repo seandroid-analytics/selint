9320386df032d7383731b4c78eb0984fd5bcef66
Filippo Bonazzi <filippo.bonazzi@aalto.fi>
2016-01-21 11:58:14 +0200

    Made the global_macros parser parse all macro levels, not just one

--- policysource/macro_plugins/global_macros.py
+++ policysource/macro_plugins/global_macros.py
@@ -23,6 +23,7 @@
 from policysource.macro import M4Macro as M4Macro, M4MacroError as M4MacroError
 import os
 import re
+import subprocess
 import logging
 
 MACRO_FILE = "global_macros"
@@ -37,6 +38,24 @@ def expects(expected_file):
         return False
 
 
+def __expand(name, tmp, m4_freeze_file):
+    """Expand the macro with the given name, using the supplied temporary
+    file and m4 freeze file."""
+    with open(tmp, "w") as mfile:
+        # Write the macro to the temporary file
+        mfile.write(name)
+    # Define the expansion command
+    command = ["m4", "-R", m4_freeze_file, tmp]
+    # Try to get the macro expansion with m4
+    try:
+        expansion = subprocess.check_output(command)
+    except subprocess.CalledProcessError as e:
+        # Log the error and change the function return value to None
+        LOG.warning("%s", e.msg)
+        expansion = None
+    return expansion
+
+
 def parse(f_to_parse, tmpdir, m4_freeze_file):
     """Parse the file and return a dictionary of macros.
 
@@ -47,6 +66,10 @@ def parse(f_to_parse, tmpdir, m4_freeze_file):
     macros = {}
     # Parse the global_macros file
     macrodef = re.compile(r'^define\(\`([^\']+)\',\s+`([^\']+)\'')
+    # Create a temporary file that will contain, at each iteration, the
+    # macro to be expanded by m4. This is better than piping input to m4.
+    tmp = os.path.join(tmpdir, "global_macrofile")
+    LOG.debug("Created temporary file \"%s\"", tmp)
     with open(f_to_parse) as global_macros_file:
         for lineno, line in enumerate(global_macros_file):
             # If the line contains a macro, parse it
@@ -54,16 +77,28 @@ def parse(f_to_parse, tmpdir, m4_freeze_file):
             if macro_match is not None:
                 # Construct the new macro object
                 name = macro_match.group(1)
-                expansion = macro_match.group(2)
-                try:
-                    new_macro = M4Macro(name, expansion, f_to_parse)
-                except M4MacroError as e:
-                    # Log the failure and skip
-                    # Find the macro line and report it to the user
-                    LOG.warning("%s", e.msg)
-                    LOG.warning("Macro \"%s\" is at %s:%s",
-                                name, f_to_parse, lineno)
+                expansion = __expand(name, tmp, m4_freeze_file)
+                if expansion:
+                    try:
+                        new_macro = M4Macro(name, expansion, f_to_parse)
+                    except M4MacroError as e:
+                        # Log the failure and skip
+                        # Find the macro line and report it to the user
+                        LOG.warning("%s", e.msg)
+                        LOG.warning("Macro \"%s\" is at %s:%s",
+                                    name, f_to_parse, lineno)
+                    else:
+                        # Add the new macro to the dictionary
+                        macros[name] = new_macro
                 else:
-                    # Add the new macro to the dictionary
-                    macros[name] = new_macro
+                    # Log the failure and skip this macro
+                    LOG.warning("Failed to expand macro \"%s\" at %s:%s",
+                                name, f_to_parse, lineno)
+    # Try to remove the temporary file
+    try:
+        os.remove(tmp)
+    except OSError:
+        LOG.debug("Trying to remove temporary file \"%s\"... failed!", tmp)
+    else:
+        LOG.debug("Trying to remove temporary file \"%s\"... done!", tmp)
     return macros
