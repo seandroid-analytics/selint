c8e82e1bb3b2948a1cda7052aacb5cdb2c7efa60
Filippo Bonazzi <filippo.bonazzi@aalto.fi>
2016-02-05 15:27:18 +0200

    Modified mapping to map all rules again

--- policysource/mapping.py
+++ policysource/mapping.py
@@ -25,10 +25,8 @@ import logging
 import re
 
 # TODO: source from config file
-# ONLY_MAP_RULES = ("allow", "auditallow", "dontaudit", "neverallow", "type_transition")
-# Only map allow for speed, since we are only analysing allow rules for now
-# TODO: remove this if/when we need to analyse more rules
-ONLY_MAP_RULES = ("allow")
+ONLY_MAP_RULES = ("allow", "auditallow", "dontaudit",
+                  "neverallow", "type_transition")
 
 
 class FileLine(object):
