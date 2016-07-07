c27367fd502d104d6da8ede0b23ce4eeefc3acd7
Elena Reshetova <elena.reshetova@intel.com>
2016-07-07 12:02:25 +0300

    Increase the trust score for core_domains bin
    core_domains bin should have a higher trust score since it contains domains and types of core AOSP

--- plugins/config/risky_rules.py
+++ plugins/config/risky_rules.py
@@ -75,7 +75,7 @@ TYPES["user_app"] = ["untrusted_app"]
 # Core system domains
 # These types cover core system services launched by the init system.
 # While not directly involved with security, these services are very important.
-SCORE_TRUST["core_domains"] = 10
+SCORE_TRUST["core_domains"] = 20
 SCORE_RISK["core_domains"] = 15
 TYPES["core_domains"] = ["adbd", "adbd_socket", "init", "init_shell",
                          "init_tmpfs", "installd", "installd_exec",
